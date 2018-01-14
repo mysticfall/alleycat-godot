1899b2636daafdb5dd0e196f855d3948ef8e83ab
Xavier Cho <mysticfallband@gmail.com>
2018-01-15 07:20:23 +0900

    Add extensions to make it easier to create IObservable<Unit>

---
+++ Source/AlleyCat/Event/ObservableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using EnsureThat;
+using JetBrains.Annotations;
+
+namespace AlleyCat.Event
+{
+    public static class ObservableExtensions
+    {
+        [NotNull]
+        public static IObservable<Unit> AsUnitObservable<T>([NotNull] this IObservable<T> observable)
+        {
+            Ensure.Any.IsNotNull(observable, nameof(observable));
+
+            return observable.Select(_ => Unit.Default);
+        }
+    }
+}
