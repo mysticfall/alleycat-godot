b2321a2fadc874b1cf0eed926a1365fd0163a13d
Xavier Cho <mysticfallband@gmail.com>
2018-01-14 20:14:44 +0900

    Dispose added disposables upon detaching from parent

--- Source/AlleyCat/Common/DisposableCollector.cs
+++ Source/AlleyCat/Common/DisposableCollector.cs
@@ -17,7 +17,7 @@ namespace AlleyCat.Common
             if (_disposables == null)
             {
                 throw new InvalidOperationException(
-                    "DisposableCollector has not been initialized yet.");
+                    "Node has been detached from parent, or already disposed.");
             }
 
             _disposables.Add(disposable);
@@ -34,12 +34,31 @@ namespace AlleyCat.Common
             _disposables = new List<IDisposable>();
         }
 
+        public override void _EnterTree()
+        {
+            base._EnterTree();
+
+            _disposables = new List<IDisposable>();
+        }
+
+        public override void _ExitTree()
+        {
+            base._ExitTree();
+
+            DisposeItems();
+        }
+
         public override void Dispose(bool disposing)
+        {
+            DisposeItems();
+
+            base.Dispose(disposing);
+        }
+
+        private void DisposeItems()
         {
             _disposables?.ForEach(d => d.Dispose());
             _disposables = null;
-
-            base.Dispose(disposing);
         }
     }
 }
