7d9b3133f282e800252218378400511f04641905
Xavier Cho <mysticfallband@gmail.com>
2018-01-19 22:50:48 +0900

    Convert "_myNode" to "MyNode" when resolving [Node] without path

--- Source/AlleyCat/Autowire/NodeAttributeProcessor.cs
+++ Source/AlleyCat/Autowire/NodeAttributeProcessor.cs
@@ -22,13 +22,24 @@ namespace AlleyCat.Autowire
 
             var node = service as Node;
 
-            Ensure.Any.IsNotNull(node, nameof(service), 
+            Ensure.Any.IsNotNull(node, nameof(service),
                 opts => opts.WithMessage(
                     "[Node] attribute is only supported on members of a Node type class."));
 
-            var path = string.IsNullOrWhiteSpace(NodePath) ? Member.Name : NodePath;
+            var path = string.IsNullOrWhiteSpace(NodePath) ? NormalizeMemberName(Member.Name) : NodePath;
 
             return ((Node) service).GetNode(path);
+        }
+
+        [NotNull]
+        protected static string NormalizeMemberName([NotNull] string name)
+        {
+            Ensure.String.IsNotNullOrWhiteSpace(name, nameof(name));
+
+            var normalized = name.StartsWith("_") ? name.Substring(1) : name;
+
+            return normalized.Length < 2 ? normalized :
+                normalized.Substring(0, 1).ToUpper() + normalized.Substring(1);
         }
     }
 }
