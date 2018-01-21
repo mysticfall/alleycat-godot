6ca84a7a1ecaeadf500d698fee18aefad55f1d54
Xavier Cho <mysticfallband@gmail.com>
2018-01-21 15:03:47 +0900

    Add base class for autowired node for convenience

---
+++ Source/AlleyCat/Autowire/AutowiredNode.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace AlleyCat.Autowire
+{
+    public class AutowiredNode : Node
+    {
+        public override void _EnterTree() => this.Prewire();
+
+        public override void _Ready() => this.Postwire();
+    }
+}
