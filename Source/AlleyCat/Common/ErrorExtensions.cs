7748d5d3edd7a666ab872ad3b85d85f21364b05b
Xavier Cho <mysticfallband@gmail.com>
2018-01-27 19:11:29 +0900

    Add extension method for Error to convert it to a C# exception

---
+++ Source/AlleyCat/Common/ErrorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Godot;
+using JetBrains.Annotations;
+
+namespace AlleyCat.Common
+{
+    public static class ErrorExtensions
+    {
+        public static void ThrowIfNecessary(
+            this Error error, [CanBeNull] Func<string, Exception> handler = null)
+        {
+            if (error == Error.Ok) return;
+
+            var code = Enum.GetName(typeof(Error), error);
+            var message = $"Operation failed with code: '{code}(error)'";
+
+            throw handler?.Invoke(message) ?? new InvalidOperationException(message);
+        }
+    }
+}
