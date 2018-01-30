095b461c7366f6673ddb693fc14eb70713c1392a
Xavier Cho <mysticfallband@gmail.com>
2018-01-31 00:46:27 +0900

    Remove workaround for godotengine/godot#15053
    Thanks @neikeq :)

--- Source/AlleyCat/Autowire/AutowiredNode.cs
+++ Source/AlleyCat/Autowire/AutowiredNode.cs
@@ -4,28 +4,18 @@ namespace AlleyCat.Autowire
 {
     public class AutowiredNode : Node
     {
-        //FIXME: Workaround for godotengine/godot#15053
-        private bool _reentry;
-
         public override void _EnterTree()
         {
             base._EnterTree();
 
             this.Prewire();
-
-            _reentry = false;
         }
 
         public override void _Ready()
         {
             base._Ready();
 
-            if (!_reentry)
-            {
-                this.Postwire();
-            }
-
-            _reentry = true;
+            this.Postwire();
         }
     }
 }
