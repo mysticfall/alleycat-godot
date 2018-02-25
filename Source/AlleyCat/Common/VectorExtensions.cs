819167e1250fe61959dbc1767ecec7c497c75352
Xavier Cho <mysticfallband@gmail.com>
2018-02-25 16:01:03 +0900

    Add extension method for Vector3/Vector2 to pretty print them in debug

---
+++ Source/AlleyCat/Common/VectorExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using EnsureThat;
+using Godot;
+using JetBrains.Annotations;
+
+namespace AlleyCat.Common
+{
+    public static class VectorExtensions
+    {
+        public static string ToFormatString(this Vector2 vector, [NotNull] string format = "###,##0.00")
+        {
+            Ensure.Any.IsNotNull(format, nameof(format));
+
+            return new StringBuilder()
+                .Append("Vector2(")
+                .Append(vector.x.ToString(format))
+                .Append(", ")
+                .Append(vector.y.ToString(format))
+                .Append(")")
+                .ToString();
+        }
+
+        public static string ToFormatString(this Vector3 vector, [NotNull] string format = "###,##0.00")
+        {
+            Ensure.Any.IsNotNull(format, nameof(format));
+
+            return new StringBuilder()
+                .Append("Vector3(")
+                .Append(vector.x.ToString(format))
+                .Append(", ")
+                .Append(vector.y.ToString(format))
+                .Append(", ")
+                .Append(vector.z.ToString(format))
+                .Append(")")
+                .ToString();
+        }
+    }
+}
