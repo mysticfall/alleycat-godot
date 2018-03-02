057a4dca51d707d2a99524ff5c590f435c419171
Xavier Cho <mysticfallband@gmail.com>
2018-03-02 15:23:08 +0900

    Extend ActionAxisInput to allow non-continuous input (i.e. mousewheel)

--- Source/AlleyCat/Control/ActionAxisInput.cs
+++ Source/AlleyCat/Control/ActionAxisInput.cs
@@ -14,13 +14,33 @@ namespace AlleyCat.Control
         [Export, NotNull]
         public string NegativeAction { get; set; } = "move_left";
 
+        [Export]
+        public bool Polling { get; set; } = true;
+
         protected float PositiveValue => Input.IsActionPressed(PositiveAction) ? 1f : 0f;
 
         protected float NegativeValue => Input.IsActionPressed(NegativeAction) ? -1f : 0f;
 
         protected float Value => PositiveValue + NegativeValue;
 
-        protected override IObservable<float> CreateRawObservable() => 
-            this.OnProcess().Select(_ => Value);
+        protected override IObservable<float> CreateRawObservable()
+        {
+            if (Polling)
+            {
+                return this.OnProcess().Select(_ => Value);       
+            }
+
+            var input = this.OnInput();
+
+            var positive = input
+                .Where(e => e.IsActionPressed(PositiveAction))
+                .Select(_ => 1f);
+
+            var negative = input
+                .Where(e => e.IsActionPressed(NegativeAction))
+                .Select(_ => -1f);
+
+            return positive.Merge(negative);
+        }
     }
 }
