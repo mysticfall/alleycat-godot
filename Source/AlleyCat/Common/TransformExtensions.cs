cfc7205e087abdec8d32a2e2a22b64d754c7ebf7
Xavier Cho <mysticfallband@gmail.com>
2018-03-04 00:54:30 +0900

    Transform.Up/Forward/Right/* should not include translation offset

--- Source/AlleyCat/Common/TransformExtensions.cs
+++ Source/AlleyCat/Common/TransformExtensions.cs
@@ -5,16 +5,16 @@ namespace AlleyCat.Common
 {
     public static class TransformExtensions
     {
-        public static Vector3 Up(this Transform transform) => transform.Xform(Axis.Up);
+        public static Vector3 Up(this Transform transform) => transform.basis.Up();
 
-        public static Vector3 Down(this Transform transform) => transform.Xform(Axis.Down);
+        public static Vector3 Down(this Transform transform) => transform.basis.Down();
 
-        public static Vector3 Forward(this Transform transform) => transform.Xform(Axis.Forward);
+        public static Vector3 Forward(this Transform transform) => transform.basis.Forward();
 
-        public static Vector3 Backward(this Transform transform) => transform.Xform(Axis.Backward);
+        public static Vector3 Backward(this Transform transform) => transform.basis.Backward();
 
-        public static Vector3 Right(this Transform transform) => transform.Xform(Axis.Right);
+        public static Vector3 Right(this Transform transform) => transform.basis.Right();
 
-        public static Vector3 Left(this Transform transform) => transform.Xform(Axis.Left);
+        public static Vector3 Left(this Transform transform) => transform.basis.Left();
     }
 }
