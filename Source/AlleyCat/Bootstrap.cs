d65c5216edc4495c5f414d1cbeadd4d13385eead
Xavier Cho <mysticfallband@gmail.com>
2018-03-17 21:22:38 +0900

    Turn off assertations in non debug build configurations

--- Source/AlleyCat/Bootstrap.cs
+++ Source/AlleyCat/Bootstrap.cs
@@ -1,10 +1,22 @@
-﻿using AlleyCat.Autowire;
+﻿using System.Diagnostics;
+using AlleyCat.Autowire;
+using EnsureThat;
 using Godot;
 
 namespace AlleyCat
 {
     public class Bootstrap : Node
     {
+        public Bootstrap()
+        {
+            Ensure.Off();
+
+            ToggleAssert();
+        }
+
+        [Conditional("DEBUG")]
+        private static void ToggleAssert() => Ensure.On();
+
         public override void _Ready()
         {
             base._Ready();
