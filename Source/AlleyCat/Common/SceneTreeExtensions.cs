297933c1586aa3cd488639e7fa680764419c9bef
Xavier Cho <mysticfallband@gmail.com>
2018-03-31 20:14:46 +0900

    Add type safe extension method for SceneTree.GetNodesInGroup

---
+++ Source/AlleyCat/Common/SceneTreeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EnsureThat;
+using Godot;
+using JetBrains.Annotations;
+
+namespace AlleyCat.Common
+{
+    public static class SceneTreeExtensions
+    {
+        [NotNull]
+        public static IEnumerable<T> GetNodesInGroup<T>([NotNull] this SceneTree sceneTree, [NotNull] string group)
+        {
+            Ensure.Any.IsNotNull(sceneTree, nameof(sceneTree));
+            Ensure.Any.IsNotNull(group, nameof(group));
+
+            return sceneTree.GetNodesInGroup(group).OfType<T>();
+        }
+    }
+}
