a1797a8aa90222a87e868a6a5189b7da7d7af6a0
Xavier Cho <mysticfallband@gmail.com>
2018-05-02 22:51:05 +0900

    Add ClosestGlobalAxis extenstion methods for Vector3
    Probably need a rewrite later.

--- Source/AlleyCat/Common/VectorExtensions.cs
+++ Source/AlleyCat/Common/VectorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 using EnsureThat;
 using Godot;
 using JetBrains.Annotations;
@@ -33,6 +34,39 @@ namespace AlleyCat.Common
                 .Append(vector.z.ToString(format))
                 .Append(")")
                 .ToString();
+        }
+
+        public static Vector2 ClosestGlobalAxis(this Vector2 axis)
+        {
+            var elements = new[] {axis.x, axis.y}.Select((v, i) => (v, i)).ToArray();
+
+            var max = elements.Max(t => Mathf.Abs(t.Item1));
+
+            var values = elements.Select(t => (max - Mathf.Abs(t.Item1), t.Item2)).OrderBy(t => t.Item1).ToArray();
+
+            values[0].Item1 = 1 * Mathf.Sign(elements[values[0].Item2].Item1);
+            values[1].Item1 = 0;
+
+            var result = values.OrderBy(t => t.Item2).Select(t => t.Item1).ToArray();
+
+            return new Vector2(result[0], result[1]);
+        }
+
+        public static Vector3 ClosestGlobalAxis(this Vector3 axis)
+        {
+            var elements = new[] {axis.x, axis.y, axis.z}.Select((v, i) => (v, i)).ToArray();
+
+            var max = elements.Max(t => Mathf.Abs(t.Item1));
+
+            var values = elements.Select(t => (max - Mathf.Abs(t.Item1), t.Item2)).OrderBy(t => t.Item1).ToArray();
+
+            values[0].Item1 = 1 * Mathf.Sign(elements[values[0].Item2].Item1);
+            values[1].Item1 = 0;
+            values[2].Item1 = 0;
+
+            var result = values.OrderBy(t => t.Item2).Select(t => t.Item1).ToArray();
+
+            return new Vector3(result[0], result[1], result[2]);
         }
     }
 }
