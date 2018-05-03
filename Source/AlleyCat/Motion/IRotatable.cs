d9503fc3f92f73ad5d27a9fc6d636d2897dc52bf
Xavier Cho <mysticfallband@gmail.com>
2018-05-03 23:31:26 +0900

    Add GetBasis(), Reset() extension methods to IRotatable

--- Source/AlleyCat/Motion/IRotatable.cs
+++ Source/AlleyCat/Motion/IRotatable.cs
@@ -1,10 +1,12 @@
 using System;
 using AlleyCat.Common;
+using EnsureThat;
 using Godot;
+using JetBrains.Annotations;
 
 namespace AlleyCat.Motion
 {
-    public interface IRotatable: IDirectional, IActivatable, IValidatable
+    public interface IRotatable : IDirectional, IActivatable, IValidatable
     {
         float Yaw { get; set; }
 
@@ -17,5 +19,25 @@ namespace AlleyCat.Motion
         Range<float> PitchRange { get; }
 
         IObservable<Vector2> OnRotationChange { get; }
+    }
+
+    public static class RotatableExtensions
+    {
+        public static void Reset([NotNull] this IRotatable rotatable)
+        {
+            Ensure.Any.IsNotNull(rotatable, nameof(rotatable));
+
+            rotatable.Rotation = Vector2.Zero;
+        }
+
+        public static Basis GetBasis([NotNull] this IRotatable rotatable)
+        {
+            Ensure.Any.IsNotNull(rotatable, nameof(rotatable));
+
+            var basis = Basis.Identity.Rotated(Vector3.Up, rotatable.Yaw);
+            var right = basis.Xform(Vector3.Right);
+
+            return basis.Rotated(right, rotatable.Pitch);
+        }
     }
 }
