797c67517cc9f3ee745cf7d0e322e6efcb54685e
Xavier Cho <mysticfallband@gmail.com>
2018-05-10 16:49:22 +0900

    Add GetTransform method extension to IDirectional

--- Source/AlleyCat/Common/IDirectional.cs
+++ Source/AlleyCat/Common/IDirectional.cs
@@ -1,4 +1,6 @@
+using EnsureThat;
 using Godot;
+using JetBrains.Annotations;
 
 namespace AlleyCat.Common
 {
@@ -11,5 +13,17 @@ namespace AlleyCat.Common
         Vector3 Up { get; }
 
         Vector3 Right { get; }
+    }
+
+    public static class DirectionalExtensions
+    {
+        public static Transform GetTransform([NotNull] this IDirectional directional)
+        {
+            Ensure.Any.IsNotNull(directional, nameof(directional));
+
+            var basis = BasisExtensions.CreateFromAxes(directional.Right, directional.Up, directional.Forward * -1);
+
+            return new Transform(basis, directional.Origin);
+        }
     }
 }
