ae8c21757401d5a79a6be67d4f4d0a75c8cebbec
Xavier Cho <mysticfallband@gmail.com>
2018-07-08 16:18:07 +0900

    Make ActionBindings an autowire context
    To prevent contained actions from being registered as global services.
    
    I'm not 100% sure if this is the right approach though.

--- Source/AlleyCat/Control/ActionBindings.cs
+++ Source/AlleyCat/Control/ActionBindings.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using AlleyCat.Action;
+using AlleyCat.Autowire;
 using AlleyCat.Common;
 
 namespace AlleyCat.Control
 {
+    [AutowireContext]
     public class ActionBindings : IdentifiableDirectory<ActionBinding>
     {
         public IReadOnlyDictionary<string, IAction> ActionMap =>
