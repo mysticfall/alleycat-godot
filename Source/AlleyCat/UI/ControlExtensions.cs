4fdbb822eb7f27c8b5947000e0c433dff044b349
Xavier Cho <mysticfallband@gmail.com>
2018-12-20 02:06:38 +0900

    Add a convenient way to get a theme's entry as Option<T>

--- Source/AlleyCat/UI/ControlExtensions.cs
+++ Source/AlleyCat/UI/ControlExtensions.cs
@@ -1,12 +1,29 @@
 using System;
 using AlleyCat.Common;
 using EnsureThat;
+using Godot;
+using LanguageExt;
+using static LanguageExt.Prelude;
 
 namespace AlleyCat.UI
 {
     public static class ControlExtensions
     {
         private const string NodeName = "ControlEventTracker";
+
+        public static Option<Color> FindColor(this Godot.Control control, string name, string type)
+        {
+            Ensure.That(control, nameof(control)).IsNotNull();
+
+            return control.HasColor(name, type) ? Some(control.GetColor(name, type)) : None;
+        }
+
+        public static Option<Font> FindFont(this Godot.Control control, string name, string type)
+        {
+            Ensure.That(control, nameof(control)).IsNotNull();
+
+            return control.HasFont(name, type) ? Some(control.GetFont(name, type)) : None;
+        }
 
         public static IObservable<MouseEnteredEvent> OnMouseEnter(this Godot.Control control)
         {
