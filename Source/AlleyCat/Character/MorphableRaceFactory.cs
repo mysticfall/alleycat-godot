368a6dad7c7dec94ecc49ac78c2110fde502b2c5
Xavier Cho <mysticfallband@gmail.com>
2019-04-28 00:21:44 +0900

    Code clean up: use static import for compact code

--- Source/AlleyCat/Character/MorphableRaceFactory.cs
+++ Source/AlleyCat/Character/MorphableRaceFactory.cs
@@ -6,6 +6,7 @@ using AlleyCat.Morph;
 using Godot;
 using LanguageExt;
 using Microsoft.Extensions.Logging;
+using static LanguageExt.Prelude;
 
 namespace AlleyCat.Character
 {
@@ -17,12 +18,11 @@ namespace AlleyCat.Character
         protected override Validation<string, MorphableRace> CreateService(
             string key, string displayName, ILoggerFactory loggerFactory)
         {
-            Option<Sex> ParseSex(string name) => Enum.TryParse(name, out Sex sex) ? Prelude.Some(sex) : Prelude.None;
+            Option<Sex> ParseSex(string name) => Enum.TryParse(name, out Sex sex) ? Some(sex) : None;
 
-            var groups = MorphRoots
-                .Bind(r => ParseSex(r.Name).Map(sex => (sex, r.GetChildComponents<IMorphGroup>())));
+            var groups = MorphRoots.Bind(r => ParseSex(r.Name).Map(sex => (sex, r.GetChildComponents<IMorphGroup>())));
 
-            return new MorphableRace(key, displayName, EquipmentSlots, Prelude.toMap(groups), loggerFactory);
+            return new MorphableRace(key, displayName, EquipmentSlots, toMap(groups), loggerFactory);
         }
     }
 }
