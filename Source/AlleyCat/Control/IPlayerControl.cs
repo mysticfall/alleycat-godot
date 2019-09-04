804da8d2062f4b486fb9df21958335e813426197
Xavier Cho <mysticfallband@gmail.com>
2019-09-04 17:39:39 +0900

    IPlayerControl should not implement IActor

--- Source/AlleyCat/Control/IPlayerControl.cs
+++ Source/AlleyCat/Control/IPlayerControl.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
-using AlleyCat.Action;
 using AlleyCat.Character;
 using AlleyCat.View;
 
 namespace AlleyCat.Control
 {
-    public interface IPlayerControl : ICharacterAware<IHumanoid>, IPerspectiveSwitcher, IActor
+    public interface IPlayerControl : ICharacterAware<IHumanoid>, IPerspectiveSwitcher
     {
         IEnumerable<IInput> Inputs { get; }
     }
