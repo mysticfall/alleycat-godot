a97d94d03364839932cdff91d5f6832cba7a9740
Xavier Cho <mysticfallband@gmail.com>
2019-10-06 00:57:03 +0900

    Add a simple MeshSurface implementation for creating custom surface

---
+++ Source/AlleyCat/Mesh/MeshSurface.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using AlleyCat.Mesh.Generic;
+using Godot;
+using LanguageExt;
+using static Godot.Mesh;
+
+namespace AlleyCat.Mesh
+{
+    public class MeshSurface : IMeshSurface
+    {
+        public string Key { get; }
+
+        public IMeshData<Vertex> Data { get; }
+
+        public uint FormatMask => Data.FormatMask;
+
+        public PrimitiveType PrimitiveType { get; }
+
+        public IEnumerable<IMeshData<MorphedVertex>> BlendShapes { get; }
+
+        public BlendShapeMode BlendShapeMode { get; }
+
+        public Option<Material> Material { get; }
+
+        public MeshSurface(
+            string key,
+            PrimitiveType primitiveType,
+            IMeshData<Vertex> data,
+            IEnumerable<IMeshData<MorphedVertex>> blendShapes,
+            BlendShapeMode blendShapeMode,
+            Option<Material> material)
+        {
+            Key = key;
+            PrimitiveType = primitiveType;
+            Data = data;
+            BlendShapes = blendShapes;
+            BlendShapeMode = blendShapeMode;
+            Material = material;
+        }
+    }
+}
